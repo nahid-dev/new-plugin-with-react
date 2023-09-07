524fbc5b3278aa70642e5939ba2c55317e4d64e4
nahid-dev <nahidv232@gmail.com>
2023-09-08 01:03:07 +0600

    start codeing

---
+++ new-plugin-with-react.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Plugin with react.js
+Author: Nahid
+Author URI: https://w3school.com
+Version: 1.0.0
+Description: Plugin with react js
+
+*/ 
+
+if(!define('ABSPATH')) : exit(); endif;
