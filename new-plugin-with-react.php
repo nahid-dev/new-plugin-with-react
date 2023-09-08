962394694dcfa573bc00beaa46a75fb477c6510d
nahid-dev <nahidv232@gmail.com>
2023-09-08 16:56:09 +0600

    initial setup done

--- new-plugin-with-react.php
+++ new-plugin-with-react.php
@@ -6,6 +6,23 @@ Author URI: https://w3school.com
 Version: 1.0.0
 Description: Plugin with react js
 
-*/ 
+*/
 
-if(!define('ABSPATH')) : exit(); endif;
+if (!defined('ABSPATH')) : exit();
+endif;
+
+// Define plugin contents
+define('WPWR_PATH', trailingslashit(plugin_dir_path(__FILE__)));
+define('WPWR_URL', trailingslashit(plugins_url('/', __FILE__)));
+
+function new_dashboard_setup()
+{
+    wp_add_dashboard_widget('new-dashboard-widget', 'New Graph Widget', 'new_dashboard_widget_callback');
+};
+
+add_action('wp_dashboard_setup', 'new_dashboard_setup');
+
+function new_dashboard_widget_callback()
+{
+    echo '<div id="new-dashboard-widget"></div>';
+}
